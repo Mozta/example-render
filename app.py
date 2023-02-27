a6cbd4039cec47aa513f930a33943aaf6fa08d5b
mozta <perezaguirrerafael@gmail.com>
2023-02-27 16:33:32 -0600

    new app

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, abort
+
+app = Flask(__name__)
+
+uri = '/api/'
+persona = {'name':'Rafael', 'matricula': '123456'}
+
+tasks = [
+    {
+        'id': 1,
+        'name': 'Cocinar algo bien sabroso',
+        'status': False
+    },
+    {
+        'id': 2,
+        'name': 'Limpiar la casa',
+        'status': False
+    },
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('index.html', data=persona)
+
+#API
+
+@app.route(uri+'/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route(uri+'/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    this_task = 0
+    for task in tasks:
+        if task['id'] == id:
+            this_task = task
+    if this_task == 0:
+        abort(404)
+    return jsonify({'task': this_task})
+
+if __name__ == '__main__':
+    app.run(debug=True)
